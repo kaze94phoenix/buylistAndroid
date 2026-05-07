5d01e75c3f7ade4f7227534b3533efd9b83addbc
kaze94phoenix <phoenix.kazekage@gmail.com>
2026-05-07 11:39:15 +0200

    updates: use model added

---
+++ app/src/main/java/com/example/buylist/models/User.java
@@ -0,0 +1,67 @@
+package com.example.buylist.models;
+
+import com.google.gson.annotations.SerializedName;
+
+public class User {
+    @SerializedName("id")
+    private int id;
+    @SerializedName("name")
+    private String name;
+    @SerializedName("email")
+    private String email;
+    @SerializedName("password")
+    private String password;
+
+    public User(){
+
+    }
+
+    public User(int id, String name, String email, String password){
+        this.id=id;
+        this.name=name;
+        this.email=email;
+        this.password=password;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
