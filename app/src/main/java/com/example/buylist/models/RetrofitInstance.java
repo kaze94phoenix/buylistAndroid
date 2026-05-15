ed15d7b406a3905a9e0b503451c3e019ff01ad58
kaze94phoenix <phoenix.kazekage@gmail.com>
2026-05-15 11:53:19 +0200

    Login api: logout function and entering as a guest prototypes

--- app/src/main/java/com/example/buylist/models/RetrofitInstance.java
+++ app/src/main/java/com/example/buylist/models/RetrofitInstance.java
@@ -10,7 +10,7 @@ public class RetrofitInstance {
 
     //Sample
     //private static final String BASE_URL = "http://127.0.0.1:8000";
-    private static final String BASE_URL = "http://192.168.43.244:8000";
+    private static final String BASE_URL = "http://127.0.0.1:8000";
     private static Retrofit retrofit;
 
     private static Gson gson = new GsonBuilder()
