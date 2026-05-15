a12ce75fabb92071e0146f85c3af05dde8ef8581
kaze94phoenix <phoenix.kazekage@gmail.com>
2026-05-15 12:01:27 +0200

    simple updates on code

--- app/src/main/java/com/example/buylist/LoginActivity.java
+++ app/src/main/java/com/example/buylist/LoginActivity.java
@@ -3,31 +3,13 @@ package com.example.buylist;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 
-import com.example.buylist.models.ApiService;
 import com.example.buylist.models.DataManager;
-import com.example.buylist.models.RetrofitInstance;
 import com.example.buylist.models.User;
-import com.google.gson.JsonObject;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.IOException;
-
-import okhttp3.ResponseBody;
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 EditText usernameTxt, passwordTxt;
--- app/src/main/java/com/example/buylist/models/RetrofitInstance.java
+++ app/src/main/java/com/example/buylist/models/RetrofitInstance.java
@@ -2,6 +2,7 @@ package com.example.buylist.models;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.Strictness;
 
 import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -13,8 +14,8 @@ public class RetrofitInstance {
     private static final String BASE_URL = "http://127.0.0.1:8000";
     private static Retrofit retrofit;
 
-    private static Gson gson = new GsonBuilder()
-            .setLenient()
+    private final static Gson gson = new GsonBuilder()
+            .setStrictness(Strictness.LENIENT)
             .create();
     public static Retrofit getRetrofitInstance() {
         if (retrofit == null) {
