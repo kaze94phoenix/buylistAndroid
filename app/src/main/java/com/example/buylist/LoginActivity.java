a24aba787a8522df0d5e0d76a917170f53112cd2
kaze94phoenix <phoenix.kazekage@gmail.com>
2026-08-05 13:41:22 +0200

    RegisterActivity Added, still to be implemented

--- app/src/main/java/com/example/buylist/LoginActivity.java
+++ app/src/main/java/com/example/buylist/LoginActivity.java
@@ -5,9 +5,11 @@ import static android.view.View.VISIBLE;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -16,6 +18,7 @@ import com.example.buylist.models.User;
 
 public class LoginActivity extends AppCompatActivity {
 EditText usernameTxt, passwordTxt;
+TextView registerLink;
 SharedPreferences preferences;
 ProgressBar progressBar;
 Button loginBtn, guestBtn;
@@ -23,16 +26,18 @@ Button loginBtn, guestBtn;
 DataManager manager;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
         manager = new DataManager(getApplicationContext());
         preferences = manager.getPreferences();
 
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
         usernameTxt = findViewById(R.id.editTextUsername);
         passwordTxt = findViewById(R.id.editTextPassword);
         loginBtn = findViewById(R.id.loginBtn);
         guestBtn = findViewById(R.id.guestBtn);
         progressBar = findViewById(R.id.progressBar);
+        registerLink = findViewById(R.id.registerLink);
 
         loginBtn.setOnClickListener(view ->{
             progressBar.setVisibility(VISIBLE);
@@ -49,6 +54,10 @@ DataManager manager;
             manager.getEditor().apply();
             startActivity(new Intent(getApplicationContext(), MainActivity.class));
             finish();
+        });
+
+        registerLink.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), RegisterActivity.class));
         });
     }
 }
